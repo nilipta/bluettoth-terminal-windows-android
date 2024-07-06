f9d59e315c2af8211ca1ed24a4009cd65a0db34c
nilipta <niliptasatapathy@gmail.com>
2024-07-06 22:04:28 +0530

    added exception for COM port

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { SerialPort, ReadlineParser } = require('serialport');
 const { exec } = require("child_process");
-const port = new SerialPort({ path: 'COM3', baudRate: 9600 })
+let port;
+
+try{
+	port = new SerialPort({ path: 'COM4', baudRate: 9600 })
+}catch(e){
+	const list = SerialPort.list();
+	console.log({list})
+}
+
 const parser = new ReadlineParser()
 
 const handleRequest = (a) => {
@@ -45,6 +53,36 @@ const handleRequest = (a) => {
 		});
 	}
 }
+
+
+
+ process.on('uncaughtException', async function(err) {
+      // handle the error safely
+	  const list = await SerialPort.list();
+	console.log({list})
+	const comportObj = list.find(x=>x.friendlyName.toLowerCase().includes('bluetooth'));
+	const comportnameStr = comportObj?.path;
+	
+	if(!comportnameStr)
+	{
+		process.exit(1);		
+	}
+	/* {
+      path: 'COM3',
+      manufacturer: 'Microsoft',
+      serialNumber: undefined,
+      pnpId: 'BTHENUM\\{00001101-0000-1000-8000-00805F9B34FB}_LOCALMFG&0000\\7&2B6A6B63&0&000000000000_00000000',
+      locationId: undefined,
+      friendlyName: 'Standard Serial over Bluetooth link (COM3)',
+      vendorId: undefined,
+      productId: undefined
+    } */
+      console.log(err)
+	  port = new SerialPort({ path: comportnameStr, baudRate: 9600 })
+	  port.pipe(parser)
+	  parser.on('data', handleRequest)
+  })
+
 
 
 port.pipe(parser)
